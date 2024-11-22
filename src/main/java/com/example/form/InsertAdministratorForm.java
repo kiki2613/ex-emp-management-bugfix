93a1504bdb1b7b5dd5a597519b58ab1357d0ed03
a.moeka <keywnm@gmail.com>
2024-11-22 14:04:58 +0900

    inseryAdministratorFormにバリデーションを追加

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
@@ -8,10 +13,20 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message = "名前を入力してください")
+	@Size(min = 1, max = 30, message = "名前は1文字以上30文字以内で入力してください")
 	private String name;
+
 	/** メールアドレス */
+	@NotBlank(message = "メールアドレスを入力してください")
+	@Size(min = 1, max = 127, message = "メールアドレスは1文字以上127文字以内で入力してください")
+	@Email(message = "メールアドレスの形式が不正です")
 	private String mailAddress;
+
 	/** パスワード */
+	@NotBlank(message = "パスワードを入力してください")
+	@Size(min = 6, max = 40, message = "パスワードは6文字以上40文字以内で設定してください")
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$", message = "パスワードは英字と数字を両方含む必要があります")
 	private String password;
 
 	public String getName() {
